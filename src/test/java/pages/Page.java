76be361634321cc13c634715e146099a7567c34c
Usame Aydogdu <aydogdusame@gmail.com>
2023-07-24 22:16:16 +0300

    first commit

---
+++ src/test/java/pages/Page.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class Page {
+    public Page(){
+        PageFactory.initElements(Driver.getDriver(),this);
+
+
+
+    }
+
+
+
+
+}
